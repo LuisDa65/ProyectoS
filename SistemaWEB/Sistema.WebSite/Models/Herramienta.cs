b567aabf85023bab4c66ccd87e0fce62a077dd59
l.tecua <l.tecua@metrologiaaplicada.com>
2021-04-03 17:57:05 -0600

    vista de Inventario de material (Index y agregar)

---
+++ SistemaWEB/Sistema.WebSite/Models/Herramienta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sistema.WebSite.Models
+{
+    public class Herramienta
+    {
+        public string Clave { get; set; }
+        public string CodigoBarra { get; set; }
+        public string Nombre { get; set; }
+        public string Color { get; set; }
+        public decimal Largo { get; set; }
+        public decimal Ancho { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FechaRegistro { get; set; }
+        public bool Activo { get; set; }
+        public string Proveedor { get; set; }
+        public string Imagen { get; set; }
+
+    }
+}
